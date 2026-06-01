cea357f31379be3703d61285f07b43a2ad568696
the_sebas0 <sebastianmarte25@gmail.com>
2026-05-31 22:22:00 -0400

    Agregar gunicorn

--- backend/run.py
+++ backend/run.py
@@ -1,13 +1,16 @@
 """Entry point de la aplicación."""
 from backend import create_app
 
-
-app = create_app()
+def crear_app():
+    app = create_app()
 
 # Crear tablas nuevas si no existen (seguro en producción, no elimina datos)
-with app.app_context():
-    from backend.models import db
-    db.create_all()
+    with app.app_context():
+        from backend.models import db
+        db.create_all()
+
+    return app
 
 if __name__ == "__main__":
+    app = crear_app();
     app.run(host="0.0.0.0", port=5001, use_reloader=False)
